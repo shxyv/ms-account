b3860dbe8873b0572af6359b2a719da90726c02b
sixiyevih <ilgar.shixiyevv@gmail.com>
2023-03-06 10:59:00 +0400

    added circuit breaker and retry logic in AccountController

--- src/main/java/com/learning/msaccount/controller/AccountController.java
+++ src/main/java/com/learning/msaccount/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.learning.msaccount.controller;
 import com.learning.msaccount.client.model.Card;
 import com.learning.msaccount.dao.entity.Account;
 import com.learning.msaccount.service.AccountService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,13 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/{id}")
+    @Retry(name = "getAccountByIdRetry")
     public Account getAccountById(@PathVariable Long id) {
         return accountService.getAccountById(id);
     }
 
     @GetMapping
+    @CircuitBreaker(name = "getAllAccountsBreaker")
     public List<Account> getAllAccounts() {
         return accountService.getAllAccounts();
     }
